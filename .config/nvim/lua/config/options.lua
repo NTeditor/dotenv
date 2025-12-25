8816031de6f75a77c28ca13347251c89ed929202
NTeditor <nteditor4@gmail.com>
2025-12-26 02:10:22 +0300

    add lazyvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,18 +1,3 @@
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_node_provider = 0
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.wrap = false
-vim.opt.scrolloff = 10
-
-vim.keymap.set({ "i", "v" }, "fd", "<Esc>", { desc = "Esc" })
-vim.keymap.set({ "t" }, "fd", "<C-\\><C-n>", { desc = "Esc" })
-vim.keymap.set({ "i", "v" }, "<Esc>", "<Nop>", { desc = "Disable Esc" })
-vim.keymap.set({ "n", "v" }, "<leader><leader>", '"+', { desc = "System clipboard" })
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
