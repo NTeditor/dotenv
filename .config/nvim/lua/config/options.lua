0a253691fa0bba72d11ce5e119ebfeda9d8ab713
NTeditor <nteditor4@gmail.com>
2025-12-26 02:04:46 +0300

    update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,15 +12,7 @@ vim.opt.number = true
 vim.opt.wrap = false
 vim.opt.scrolloff = 10
 
-vim.keymap.set({ "i", "v" }, "df", "<Esc>", { desc = "Esc" })
 vim.keymap.set({ "i", "v" }, "fd", "<Esc>", { desc = "Esc" })
-vim.keymap.set({ "t" }, "df", "<C-\\><C-n>", { desc = "Esc" })
 vim.keymap.set({ "t" }, "fd", "<C-\\><C-n>", { desc = "Esc" })
 vim.keymap.set({ "i", "v" }, "<Esc>", "<Nop>", { desc = "Disable Esc" })
 vim.keymap.set({ "n", "v" }, "<leader><leader>", '"+', { desc = "System clipboard" })
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		require("lazy").update({ show = false })
-	end,
-})
